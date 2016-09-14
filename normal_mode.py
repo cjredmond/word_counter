64bcdf096d134eeeffa58cd363fc239ddb71383b
cjredmond <connor.redmond@gmail.com>
2016-09-14 16:33:12 -0400

    made list of top 20 words and printed

--- normal_mode.py
+++ normal_mode.py
@@ -1,6 +1,9 @@
 with open("sample.txt") as better_open_file:
-    better_contents = better_open_file.read().lower()
-
+    better_contents = better_open_file.read().lower() #.replace(",", "")
+excluded = "'1234567890,.?;:'[]{}!"
+for x in excluded:
+    if x in better_contents:
+        better_contents = better_contents.replace(x,"")
 ls = better_contents.split()
 
 
@@ -15,6 +18,12 @@ for word in ls:
     else:
         dict[word] = 1
 
-# print(dict)
-# print(dict.items())
-print(sorted(dict.items(), key=lambda x: x[1] ))
+new = sorted(dict.items(), key=lambda x: x[1] )
+
+top_group = []
+
+
+for number in range(19):
+    top_group.append(new.pop())
+for number in range(19):
+    print(top_group[number])
