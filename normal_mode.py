2171cf94b7276dbda05423b6b522287f6918bfdd
cjredmond <connor.redmond@gmail.com>
2016-09-14 14:35:23 -0400

    initial commit

---
+++ normal_mode.py
@@ -0,0 +1,11 @@
+with open("sample.txt") as better_open_file:
+    better_contents = better_open_file.read()
+
+ls = better_contents.split()
+word_count = {}
+
+for word in ls:
+    if word in word_count
+
+
+print(ls)
