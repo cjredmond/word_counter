9c0b0c640651b7799f7185b2a5b22baf685cb7ef
cjredmond <connor.redmond@gmail.com>
2016-09-14 15:47:18 -0400

    added lambda sort for dictionary

--- normal_mode.py
+++ normal_mode.py
@@ -1,11 +1,20 @@
 with open("sample.txt") as better_open_file:
-    better_contents = better_open_file.read()
+    better_contents = better_open_file.read().lower()
 
 ls = better_contents.split()
-word_count = {}
+
+
+
+new = []
+dict = {}
+
 
 for word in ls:
-    if word in word_count
+    if word in dict.keys():
+        dict[word] += 1
+    else:
+        dict[word] = 1
 
-
-print(ls)
+# print(dict)
+# print(dict.items())
+print(sorted(dict.items(), key=lambda x: x[1] ))
